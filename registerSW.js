65320f3c3dc2628a06731b01ecc44e22693a4367
ateliray <ateliray.ai@gmail.com>
2026-05-26 08:24:44 +0330

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/paradise/sw.js', { scope: '/paradise/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
